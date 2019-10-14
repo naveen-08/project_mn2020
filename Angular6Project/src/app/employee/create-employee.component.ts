701af7e9af425b5d75a78f585de557e7377eb4a1
navee <naveen@prakat.in>
2019-10-14 19:48:04 +0530

    validation

--- Angular6Project/src/app/employee/create-employee.component.ts
+++ Angular6Project/src/app/employee/create-employee.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl} from '@angular/forms';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-employeeForm:FormGroup;
-  constructor() { }
+  employeeForm: FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm=new FormGroup({
-      fullName:new FormControl(),
-      email:new FormControl(),
+    this.employeeForm =this.fb.group({
+      fullName: ['',Validators.required],
+      email: [''],
+      skills:this.fb.group({
+        skillsName:['',Validators.required],
+        Experience:[''],
+        proficiency:['beginner']
+      })
     });
-    
+
   }
-  onSubmit():void{
-    console.log(this.employeeForm.value);
+  loadDataMethod():void{
+    this.employeeForm.patchValue({
+      fullName:"naveen",
+      email:'naveen@gamail.cion',
+      skills:{
+        skillsName:'java',
+        Experience:'1',
+        proficiency:'Intermediate'
+     }
+    })
+  }
+  onSubmit(): void {
+    console.log(this.employeeForm);
+    console.log(this.employeeForm.get('fullName').value);
   }
 
 }
